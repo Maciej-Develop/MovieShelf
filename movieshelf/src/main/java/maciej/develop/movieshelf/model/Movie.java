ff6eb73abaae14601dd5fbb2d8c4c1e7cbf4f9a7
Maciej Piechowski <58206@etu.he2b.be>
2025-02-04 13:29:44 +0100

    added dependencies + model movie created

---
+++ movieshelf/src/main/java/maciej/develop/movieshelf/model/Movie.java
@@ -0,0 +1,34 @@
+package maciej.develop.movieshelf.model;
+
+import java.sql.Blob;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+
+    private String overview;
+
+    private LocalDate releaseDate;
+
+    @Lob
+    private Blob poster;
+
+}
