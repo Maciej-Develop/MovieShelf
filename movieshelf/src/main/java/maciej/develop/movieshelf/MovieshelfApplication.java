d590e1a2b9878cc0fd1703c25d8b43d1c002f1f9
Maciej Piechowski <58206@etu.he2b.be>
2025-02-04 11:57:37 +0100

    project created

---
+++ movieshelf/src/main/java/maciej/develop/movieshelf/MovieshelfApplication.java
@@ -0,0 +1,13 @@
+package maciej.develop.movieshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieshelfApplication.class, args);
+	}
+
+}
---
+++ movieshelf/src/test/java/maciej/develop/movieshelf/MovieshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package maciej.develop.movieshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
