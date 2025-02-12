b7f6cb107794c93207750bff2958a5f3e022df33
Maciej Piechowski <maciej.piechowski.dev@gmail.com>
2025-02-12 22:42:45 +0100

    restart

--- movieshelf/src/main/java/maciej/develop/movieshelf/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package maciej.develop.movieshelf.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import maciej.develop.movieshelf.model.Movie;
-
-public interface MovieRepository extends CrudRepository<Movie, Long> {
-
-}
