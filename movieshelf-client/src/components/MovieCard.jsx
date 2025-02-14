50a7089e4599f7a28685baedf8bcd8d2d4658e35
Maciej Piechowski <maciej.piechowski.dev@gmail.com>
2025-02-14 14:23:27 +0100

    movie card and home

--- movieshelf-client/src/components/MovieCard.jsx
+++ movieshelf-client/src/components/MovieCard.jsx
@@ -1,10 +1,10 @@
 function MovieCard({movie}) {
-    return <div className="movie-card">
-        <div className="movie-poster">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+    return <div className="card p-0">
+        <div className="card-img-top">
+            <img className="img-fluid rounded-top mb-3" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
         </div>
-        <div className="movie-info">
-            <h3>{movie.title}</h3>
+        <div className="card-info mb-3">
+            <h5>{movie.title}</h5>
         </div>
     </div>
 }
--- movieshelf-client/src/pages/Home.jsx
+++ movieshelf-client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
         }, []);
 
     return <div className="home">
-        <div className="movie-grid">
+        <div className="row row-cols-1 row-cols-md-5 gap-4 justify-content-center">
             {movies.map((movie) => (
                 <MovieCard movie={movie} key={movie.id}/>
             ))}
