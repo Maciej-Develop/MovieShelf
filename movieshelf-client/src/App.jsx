900ce7109d96ae013bdf21dce9d426eb87d255f2
Maciej Piechowski <maciej.piechowski.dev@gmail.com>
2025-02-20 17:31:04 +0100

    basic style detail

--- movieshelf-client/src/App.jsx
+++ movieshelf-client/src/App.jsx
@@ -9,7 +9,7 @@ import './App.css';
 function App() {
 
   return (
-    <div className="main">
+    <div>
       <NavBar />
       <main className="container py-5">
         <Routes>
--- movieshelf-client/src/components/MovieDetail.jsx
+++ movieshelf-client/src/components/MovieDetail.jsx
@@ -6,27 +6,28 @@ function MovieDetail({movie}) {
         return "bg-danger";
     };
 
-    return <div>
+    return <>
     <div>
-      <div>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
-        <h5>{movie.tagline}</h5>
-        <p><strong>Score:</strong> <span className={`badge ${getBadgeClass(movie.vote_average)}`}>{movie.vote_average.toFixed(1)}</span> ({movie.vote_count} votes)</p>
-      </div>
-      
-    <div>
-        <div>
-          <h1>{movie.title}</h1>
-          <p><strong>Overview:</strong></p>
-          <p>{movie.overview}</p>
-          <p><strong>Release Date:</strong> {movie.release_date}</p>
-          <p><strong>Status:</strong> {movie.status}</p>
-          <p><strong>Length:</strong> {movie.runtime} minutes</p>
-          <p><strong>Budget:</strong> ${movie.budget.toLocaleString()}</p>
+      <div className="row">
+        <div className="col-3 border">
+          <img className="img-fluid" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+          <h5>{movie.tagline}</h5>
+          <p><strong>Score:</strong> <span className={`badge ${getBadgeClass(movie.vote_average)}`}>{movie.vote_average.toFixed(1)}</span> ({movie.vote_count} votes)</p>
+        </div>
+        <div className="col border">
+          <div>
+            <h1>{movie.title}</h1>
+            <p><strong>Overview:</strong></p>
+            <p>{movie.overview}</p>
+            <p><strong>Release Date:</strong> {movie.release_date}</p>
+            <p><strong>Status:</strong> {movie.status}</p>
+            <p><strong>Length:</strong> {movie.runtime} minutes</p>
+            <p><strong>Budget:</strong> ${movie.budget.toLocaleString()}</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </>
 }
 
 export default MovieDetail;
