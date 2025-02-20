13240bb068376e58e28dfdeac85fd9b674568d3e
Maciej Piechowski <maciej.piechowski.dev@gmail.com>
2025-02-20 14:48:40 +0100

    put test back

--- movieshelf-client/src/services/api.js
+++ movieshelf-client/src/services/api.js
@@ -2,7 +2,8 @@
 const BASE_URL="http://localhost:8080"
 
 export const getDiscoverMovies = async (page) => {
-    const response = await fetch(`${BASE_URL}/api/movies?page=${page}`);
+    // const response = await fetch(`${BASE_URL}/api/movies?page=${page}`);
+    const response = await fetch(`${BASE_URL}/api/test/movies`);
     const data = await response.json();
     return data.results;
 }
