f689f69a504a8a72a5404c579b5102fa3254b3c5
Maciej Piechowski <maciej.piechowski.dev@gmail.com>
2025-02-20 12:38:51 +0100

    navbar style

--- movieshelf-client/src/App.jsx
+++ movieshelf-client/src/App.jsx
@@ -2,6 +2,8 @@ import {Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
 import MovieDetailPage from './pages/MovieDetailPage';
 import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
 
--- movieshelf-client/src/components/NavBar.jsx
+++ movieshelf-client/src/components/NavBar.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
 
 function NavBar() {
-    return <nav>
-    <div>
-        <Link to="/">
-            Movie Shelf
-        </Link>
-        <div>
-            <Link to="/">Home</Link>
-        </div>
+    return <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container-fluid">
+      <Link to="/" className="navbar-brand">MovieShelf</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+        </ul>
+      </div>
     </div>
-</nav>
+  </nav>
 }
 
 export default NavBar;
