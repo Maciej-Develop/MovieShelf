41c08a7a18edb71589afa3e6bf3daa848005972b
Maciej Piechowski <maciej.piechowski.dev@gmail.com>
2025-03-17 14:11:25 +0100

    start of jwt token authentication implementation

---
+++ movieshelf/src/main/java/maciej/develop/movieshelf/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package maciej.develop.movieshelf.security;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import maciej.develop.movieshelf.model.User;
+import maciej.develop.movieshelf.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username + " not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                Collections.emptyList());
+    }
+}
